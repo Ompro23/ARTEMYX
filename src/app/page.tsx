927dd15cde62df1fbab698c07783d68e63f2cf03
Omcodes23 <omvataliya23@gmail.com>
2024-09-30 17:16:39 +0530

    hello

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,19 @@
-import Image from "next/image";
 import Hero from "@/components/Hero"; // Adjust the path as necessary
 import Info from "@/components/Info"; // Adjust the path as necessary
 import Review from "@/components/Review";
 import Wave from "@/components/Wave";
-import Tooltip from "@/components/Tooltip";
-import ThreeDcard from "@/components/Threedcard";
 import Footer from "@/components/Footer";
-
-
 
 export default function Home() {
   return (
-    < >
+    <>
       <Hero />
       <Info />
-      <Review/>
-      <Wave/>
-      {/* <ThreeDcard/> */}
-      <Footer/>
+      <Review />
+      <Wave />
+      <Footer />
     </>
   );
 }
 
-
-// className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]"
+// className=&quot;min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]&quot;
