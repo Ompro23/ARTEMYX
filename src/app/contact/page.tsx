8d7dae649bafdc30f4dea1952881d1291cc5ba1e
Omcodes23 <omvataliya23@gmail.com>
2024-09-30 18:51:54 +0530

    h

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -53,7 +53,9 @@ export function BackgroundBeamsDemo() {
                 <h1 className="relative z-10 text-4xl sm:text-5xl md:text-7xl bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600 text-center font-sans font-bold">
                     Contact Us
                 </h1>
-               
+                <p className="text-neutral-500 max-w-lg mx-auto my-2 text-sm text-center relative z-10">
+                    Have any questions or concerns? 
+                </p>
                 <form onSubmit={handleSubmit} className="relative z-10 mt-4">
                     <input
                         type="email"
