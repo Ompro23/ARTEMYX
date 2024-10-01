ca06156e8f5d9d0abaa999605e0a4b6e4cf2580d
Omcodes23 <omvataliya23@gmail.com>
2024-10-01 20:04:07 +0530

    hie

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -49,7 +49,7 @@ const content = [
             "Enjoy the AR Experience",
         content: (
             <div className="h-full w-full bg-[linear-gradient(to_bottom_right,var(--purple-500),var(--pink-500))] flex items-center justify-center text-white mb-2 sm:mb-0">
-                
+                hi
             </div>
         ),
     },
