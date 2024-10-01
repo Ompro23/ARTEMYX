ce9300f640247df12e2947b545f15752632c3955
Omcodes23 <omvataliya23@gmail.com>
2024-10-01 20:06:22 +0530

    Final Production commit : 8:30

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -48,7 +48,7 @@ const content = [
         description:
             "Enjoy the AR Experience",
         content: (
-            <div className="h-full w-full bg-[linear-gradient(to_bottom_right,var(--purple-500),var(--pink-500))] flex items-center justify-center text-white mb-2 sm:mb-0">
+            <div className="">
                 hi
             </div>
         ),
