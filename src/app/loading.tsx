501b7f18b7f1d161007b7fd30566a6ad7fa17315
Om vataliya <109670967+Omcodes23@users.noreply.github.com>
2024-12-08 16:22:05 +0530

    added the loader

---
+++ src/app/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+    return (
+        <div className="flex items-center justify-center h-screen bg-black">
+            <img src="/logo/fff.jpg" alt="Loading" className="h-32 w-32" />
+        </div>
+    );
+}
