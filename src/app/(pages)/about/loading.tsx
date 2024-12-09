3460d41aff93d6605b9085192d904afcfdea632f
Om vataliya <109670967+Omcodes23@users.noreply.github.com>
2024-12-09 22:23:53 +0530

    end of the day commit

--- src/app/(pages)/about/loading.tsx
+++ src/app/(pages)/about/loading.tsx
@@ -1,7 +1,10 @@
-export default function Loading() {
-    return (
-        <div className="flex items-center justify-center h-screen bg-black">
-            <img src="/logo/fff.jpg" alt="Loading" className=" w-auto" />
-        </div>
-    );
+import Loading from '../loading/loading';
+import React from 'react'
+
+function loading() {
+  return (
+    <Loading/>    
+)
 }
+
+export default loading
--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,7 +1,7 @@
 export default function Loading() {
     return (
         <div className="flex items-center justify-center h-screen bg-black">
-            <img src="/logo/fff.jpg" alt="Loading" className=" w-auto" />
+            <img src="/logo/fff.png" alt="Loading" className=" w-auto" />
         </div>
     );
 }
